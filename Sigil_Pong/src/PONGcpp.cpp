809e9e20443516a6f489016c8bf6fcdd5565e997
ElSicher <ikernoya4@gmail.com>
2019-09-10 13:06:21 -0300

    menu agregado, pero necesita reparacion

--- Sigil_Pong/src/PONGcpp.cpp
+++ Sigil_Pong/src/PONGcpp.cpp
@@ -1,17 +1,14 @@
 ﻿#include "sl.h"
+#include <string>
+using namespace std;
 #define screenHeight 720
 #define screenWidth 1280
 enum GAMESTATE {
 	menu,
-	gamePvP,
-	gameAI,
-	end
+	game,
+	gameOver
 }state;
-struct Vector2
-{
-	int x;
-	int y;
-};
+
 struct Players {
 	int width;
 	int height;
@@ -30,13 +27,19 @@ struct Ball {
 Players player1;
 Players player2;
 Ball ball;
+
 bool p1ColWithBall = true;
 bool p2ColWithBall = true;
 bool start = false;
 bool initStart = true;
+bool winnerP1 = false;
+bool winnerP2 = false;
 
+int scoreP1 = 0;
+int scoreP2 = 0;
 void main()
 {
+	state = game;
 	//players
 	player1.x = 100;
 	player1.y = 360;
@@ -54,98 +57,132 @@ void main()
 
 	//game
 	slWindow(screenWidth, screenHeight, "PONG v1.0", false);
+	slSetFont(slLoadFont("ttf/whitrabt.ttf"), 24);
 	slSetTextAlign(SL_ALIGN_CENTER);
-
 	while (!slShouldClose() && !slGetKey(SL_KEY_ESCAPE))
 	{ 
-		//init
-		if (initStart == true)
-		{
-			p1ColWithBall = true;
-			p2ColWithBall = true;
-			initStart = false;
-		}
-		//inputs
-		if (slGetKey('W')) player1.y += 8;	
-		if (slGetKey('S')) player1.y -= 8;
-		if (slGetKey(SL_KEY_UP)) player2.y += 8;
-		if (slGetKey(SL_KEY_DOWN)) player2.y -= 8;
-		
-		if (slGetKey(32)) start = true;		//space = 32 in ASCII
+			switch (state)
+			{
+			case game:
+				while (!slShouldClose() && !slGetKey(SL_KEY_ESCAPE))
+				{
+					//init
+					if (initStart == true)
+					{
+						p1ColWithBall = true;
+						p2ColWithBall = true;
+						initStart = false;
+					}
+					//inputs
+					if (slGetKey('W')) player1.y += 8;
+					if (slGetKey('S')) player1.y -= 8;
+					if (slGetKey(SL_KEY_UP)) player2.y += 8;
+					if (slGetKey(SL_KEY_DOWN)) player2.y -= 8;
+
+					if (slGetKey(32)) start = true;		//space = 32 in ASCII
+
+					if (start == true)
+					{
+						ball.x += ball.ballSpeedX;
+						ball.y += ball.ballSpeedY;
+
+					}
+
+					//drawings
+					slSetFontSize(24);
+					string score1 = "Player 1: " + to_string(scoreP1);
+					string score2 = "Player 2: " + to_string(scoreP2);
+					slText(100, 690, score1.c_str());
+					slText(1180, 690, score2.c_str());
+
+
+					slRectangleFill(player1.x, player1.y, player1.width, player1.height);
+					slRectangleFill(player2.x, player2.y, player2.width, player2.height);
+					slCircleFill(ball.x, ball.y, ball.radius, ball.vert);
+
+
+					//collisions
+					if (p2ColWithBall && (ball.x + ball.radius) >= (player2.x - player2.width / 2) && (ball.x + ball.radius) <= (player2.x + player2.width / 2) && (ball.y >= (player2.y - player2.height / 2)) && (ball.y <= (player2.y + player2.height / 2)))
+					{
+						ball.ballSpeedX *= -1.0f;
+						p2ColWithBall = false;
+						p1ColWithBall = true;
+					}
+					if (p1ColWithBall && (ball.x - ball.radius) <= (player1.x + player1.width / 2) && (ball.x + ball.radius) >= (player1.x - player1.width / 2) && (ball.y >= (player1.y - player1.height / 2)) && (ball.y <= (player1.y + player1.height / 2)))
+					{
+						ball.ballSpeedX *= -1.0f;
+						p1ColWithBall = false;
+						p2ColWithBall = true;
+					}
+					if (player1.y - player1.height / 2 <= 0)
+					{
+						player1.y = player1.height / 2;
+					}
+					else if (player1.y + player1.height / 2 >= screenHeight)
+					{
+						player1.y = 720 - player1.height / 2;
+					}
+					if (player2.y - player2.height / 2 <= 0)
+					{
+						player2.y = player2.height / 2;
+					}
+					else if (player2.y + player2.height / 2 >= screenHeight)
+					{
+						player2.y = 720 - player2.height / 2;
+					}
+					if (ball.y + ball.radius >= screenHeight)
+					{
+						ball.ballSpeedY *= -1.0f;
+					}
+					if (ball.y - ball.radius <= 0)
+					{
+						ball.ballSpeedY *= -1.0f;
+					}
+
+					//rules
+					if (ball.x + ball.radius >= screenWidth)
+					{
+						ball.x = screenWidth / 2;
+						ball.y = screenHeight / 2;
+						start = false;
+						p1ColWithBall = true;
+						p2ColWithBall = true;
+						initStart = true;
+						scoreP1++;
+					}
+					if (ball.x - ball.radius <= 0)
+					{
+						ball.x = screenWidth / 2;
+						ball.y = screenHeight / 2;
+						start = false;
+						p1ColWithBall = true;
+						p2ColWithBall = true;
+						initStart = true;
+						scoreP2++;
+					}
+					if (scoreP1 == 3)
+					{
+						winnerP1 = true;
+						state = gameOver;
+						scoreP1 = 0;
+						scoreP2 = 0;
+						initStart = false;
+					}
+					if (scoreP1 == 3)
+					{
+						winnerP2 = true;
+						state = gameOver;
+						scoreP1 = 0;
+						scoreP2 = 0;
+						initStart = false;
+					}
+				}
+				break;
 			
-		if (start == true)
-		{
-			ball.x += ball.ballSpeedX; 
-			ball.y += ball.ballSpeedY;
+			}
 
-		}
-		
-		//drawings
-		slRectangleFill(player1.x, player1.y, player1.width, player1.height);
-		slRectangleFill(player2.x, player2.y, player2.width, player2.height);
-		slCircleFill(ball.x, ball.y, ball.radius, ball.vert);
+			slRender();
 
-		
-		//collisions
-		if (p2ColWithBall && (ball.x + ball.radius) >= (player2.x - player2.width /2) && (ball.x + ball.radius) <= (player2.x + player2.width / 2) && (ball.y >=(player2.y-player2.height/2)) && (ball.y <= (player2.y + player2.height / 2)))
-		{
-			ball.ballSpeedX *= -1.0f; 
-			p2ColWithBall = false;
-			p1ColWithBall = true;
-		}
-		if (p1ColWithBall&&(ball.x - ball.radius) <= (player1.x + player1.width / 2) && (ball.x + ball.radius) >= (player1.x - player1.width/2) && (ball.y >= (player1.y - player1.height/2)) && (ball.y <= (player1.y + player1.height /2)))
-		{
-			ball.ballSpeedX *= -1.0f;
-			p1ColWithBall = false;
-			p2ColWithBall = true;
-		}
-		if (player1.y - player1.height / 2 <= 0)
-		{
-			player1.y = player1.height / 2;
-		}
-		else if (player1.y + player1.height / 2 >= screenHeight)
-		{
-			player1.y = 720 - player1.height / 2;
-		}
-		if (player2.y - player2.height / 2 <= 0)
-		{
-			player2.y = player2.height / 2;
-		}
-		else if (player2.y + player2.height / 2 >= screenHeight)
-		{
-			player2.y = 720 - player2.height / 2;
-		}
-		if (ball.y + ball.radius >= screenHeight)
-		{
-			ball.ballSpeedY *= -1.0f;
-		}
-		if (ball.y - ball.radius <= 0)
-		{
-			ball.ballSpeedY *= -1.0f;
-		}
-
-		//rules
-		if (ball.x + ball.radius >= screenWidth)
-		{
-			ball.x = screenWidth / 2;
-			ball.y = screenHeight / 2;
-			start = false;
-			p1ColWithBall = true;
-			p2ColWithBall = true;
-			initStart = true;
-		}
-		if (ball.x - ball.radius <= 0)
-		{
-			ball.x = screenWidth / 2;
-			ball.y = screenHeight / 2;
-			start = false;
-			p1ColWithBall = true;
-			p2ColWithBall = true;
-			initStart = true;
-		}
-
-	
-		slRender();
 	}
 	slClose();
 
